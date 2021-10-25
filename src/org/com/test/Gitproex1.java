afdc5a48027bcbb5ded75571ae8783870c5bdf9b
Josephinejegan <josephinejegan5@gmail.com>
2021-10-25 07:10:03 -0700

    joshphine

---
+++ src/org/com/test/Gitproex1.java
@@ -0,0 +1,15 @@
+package org.com.test;
+
+public class Gitproex1 {
+	private void empId() {
+	
+		
+		System.out.println("empid is:12345");
+	}
+	public static void main(String[] args) {
+		
+		System.out.println("welcome");
+	}
+	}
+
+
